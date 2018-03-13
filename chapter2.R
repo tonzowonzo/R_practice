11cba156c2e85f6a32a17c4cff90331e70340472
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-03-13 18:33:57 +0100

    Add files via upload

---
+++ chapter2.R
@@ -0,0 +1,70 @@
+# R book chapter 2, Essentials of R.
+
+# Addition.
+1 + 2
+
+# Subtraction.
+3 - 1
+
+# Multiplication
+3 * 3
+
+# Division
+5/2
+
+# Multiple expressions in one line.
+2+3; 5-3; 1-2
+
+# log base e
+log(10)
+
+# log base 10
+log10(6)
+
+# log base n of x
+log(2, 10)
+
+# Pi
+pi
+
+# Sin
+sin(pi/2)
+
+# Cos
+cos(pi/2)
+
+# Tan
+tan(pi/2)
+
+# Exp
+exp(10)
+
+# Square root
+sqrt(10)
+
+# Factorial
+factorial(5)
+
+#binomial coefficients
+choose(3, 10)
+
+# smallest integer < x
+floor(3.52)
+
+# largest integer > x
+ceiling(3.52)
+
+# Truncate / round
+trunc(3.432)
+
+# Round
+round(5.32175632, digits=5)
+
+# scientific notation
+signif(532432.33432423634, digits=6)
+
+# Generate n random numbers between 0 and 1 on uniform dist
+runif(20)
+
+# absolute number
+abs(-10)
