ec237a806cca2be5ed81eb8d785dac902d180030
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-04-17 11:49:50 +0200

    Add files via upload

--- week1.R
+++ week1.R
@@ -9,3 +9,45 @@ a * c
 a - b
 
 # Functions.
+# We need to use parenthesis.
+ls() # This is a function.
+
+# Log func.
+log(8)
+log(a)
+
+# Exp
+exp(1)
+log(exp(1))
+
+# Help system.
+help("log")
+?log
+
+# Arguments of log
+args(log)
+
+# Change args
+log(x=2, base=2)
+
+# Sum to 1000.
+n <- 1000
+n*(n+1)/2
+
+# Log base 10
+log10(sqrt(100))
+
+# Data types.
+class(ls) # Function
+
+# Dataframes
+# Like tables and very useful because of merging etc
+library(dslabs)
+data("murders")
+class(murders)
+str(murders) # Structure of dataframe
+head(murders) # Head of df
+
+
+# Clear memory
+gc()
